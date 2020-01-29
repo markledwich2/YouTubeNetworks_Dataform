125e0473c551ffa9bbb8644a9adbaea3cc6ae4cd
Mark Ledwich <markoledwich@gmail.com>
2020-01-29 09:41:43 +0000

    rec export

--- includes/stage.js
+++ includes/stage.js
@@ -2,9 +2,9 @@ function create(table) {
     return `create table if not exists ${table} (v Variant)`;
 }
 
-function copy(table, sourcePath) {
+function copy(table, sourcePath, stage) {
     return `
-    copy into ${table} from @public.yt/${sourcePath}
+    copy into ${table} from @${stage ? stage : 'public.yt'}/${sourcePath}
     file_format=(type=json)
 `;
 }
