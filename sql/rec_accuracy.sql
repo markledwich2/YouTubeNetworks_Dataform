3ff2a23ced0eaf0b6721f09fee119bc9a38d9edd
Mark Ledwich <mark@ledwich.com.au>
2020-04-10 02:11:53 +1000

    user scraping wheighted random videos to watch in each ideology

--- sql/rec_accuracy.sql
+++ sql/rec_accuracy.sql
@@ -5,6 +5,7 @@ select from_date
      , impressions_estimate
      , from_ideology
      , to_ideology
+     , impressions_neutral
 from rec_export_vs_estimate
 where from_date >= '2019-11-01'::date
 order by from_date, to_channel_hash, from_channel_hash
