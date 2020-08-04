8189b1a0625e316dc92b64a63415cff8c0153ba0
Mark Ledwich <mark@ledwich.com.au>
2020-08-04 18:33:44 +1000

    cannel review | seperate tags to make this cleaner to read

--- sql/channel_review.sql
+++ sql/channel_review.sql
@@ -1,10 +1,10 @@
 select channel_title
-     , tags
+     , array_to_string(tags, '|') as tags
      , channel_id
      , lr
      , relevance
      , r.value:lr::string as reviewer_lr
-     , r.value:tags::array as reviewer_tags
+     , array_to_string( r.value:tags::array, '|') as reviewer_tags
      , r.value:main_channel_id::string as reviewer_main_channel_id
     , r.value:relevance::double as reviewer_relevance
 , left(r.value:email::string, 3) as reviewer_code
