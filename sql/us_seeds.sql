0bafe8e9f05b4f0a3d51d99ccc4b6f7fbcc7201c
Mark Ledwich <mark@ledwich.com.au>
2020-08-31 18:21:39 +1000

    change userscrape to work off tags

--- sql/us_seeds.sql
+++ sql/us_seeds.sql
@@ -1,8 +1,7 @@
--- videos with view rank within an  channel in the last year
+-- videos with view rank within a channel in the last year
 with c_rand as (
-  select channel_id, gen_no, uniform(0, 10000000000, random()) / 10000000000 as rand
-  from (select seq4() as gen_no from table (generator(rowcount => :videos_per_ideology
-                                                                    /  10)))
+  select channel_id, gen_no, uniform(0, 10000000000, random())/10000000000 as rand
+  from (select seq4() as gen_no from table (generator(rowcount => :videos_per_tag / 10)))
      , (select distinct channel_id from channel_accepted) c
 )
 
@@ -10,7 +9,7 @@ with c_rand as (
    , v1 as (
   select c.channel_title
        , c.channel_id
-       , c.ideology
+       , c.tags
        , video_id
        , video_title
        , views
@@ -18,38 +17,39 @@ with c_rand as (
        , sum(views) over (partition by c.channel_id order by views rows unbounded preceding) as views_running
        , sum(views) over (partition by c.channel_id) as channel_views_total
   from video_latest v
-         left join channel_accepted c on v.channel_id = c.channel_id
+         left join channel_accepted c on v.channel_id=c.channel_id
   where c.channel_id is not null
-    and views > 0
-    and upload_date > dateadd(day, -365, (select max(upload_date) from video_latest))
+    and views>0
+    and upload_date>dateadd(day, -365, (select max(upload_date) from video_latest))
 )
 
-   -- v1 + last running_views with in ideology
+   -- v1 + last running_views
    , v2 as (
   select v1.*
        , r.gen_no
-       , r.rand * channel_views_total as rand_views
+       , r.rand*channel_views_total as rand_views
        , coalesce(lag(views_running) over (partition by v1.channel_id, gen_no order by views), 0) as last_views_running
   from v1
-         left join c_rand r on v1.channel_id = r.channel_id
+         left join c_rand r on v1.channel_id=r.channel_id
     qualify
-      rand_views > last_views_running and rand_views <= views_running -- chosen randomly proportional to views
-  --and video_id_no = 1 -- remove duplicate videos
+      rand_views>last_views_running and rand_views<=views_running -- chosen randomly proportional to views
 )
 
    , v3 as (
-  -- top 50 distinct videos spread across channels in each ideology group
+  -- distinct videos
   select *
        , row_number() over (partition by video_id order by gen_no) as video_id_no
   from v2
-    qualify video_id_no = 1 --and ideology_rank < 50
+    qualify video_id_no=1
 )
-
    , v4 as (
-  select *
-       , row_number() over (partition by ideology order by gen_no desc) as ideology_rank -- arbitrarily chose x within and idelogy
-  from v3 qualify ideology_rank <= :videos_per_ideology
+  select tag
+       , row_number() over (partition by tag order by gen_no desc) as ideology_rank -- arbitrarily chose x within and idelogy
+       , v3.*
+  from us_tags t
+         left join v3 on array_contains(t.tag::variant, v3.tags)
+    qualify ideology_rank <= :videos_per_tag
 )
 select *
 from v4
-order by ideology, video_id
+order by tag, video_id
