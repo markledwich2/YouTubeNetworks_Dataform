c2c949bb1305a5e36acfd42e90f33c832337fd3a
Mark Ledwich <google-oauth2|112278319354366421989>
2020-09-23 03:05:45 +0000

    video stats, new sam reviews

--- includes/channel.js
+++ includes/channel.js
@@ -11,4 +11,23 @@ function recCatColumns() {
     return categories.map(c => recCatPair(c)).join(',')
 }
 
-module.exports = { categories, recCatColumns };
+function periodStatsObject(agoCol, valueCol, alias) {
+  function c(agoA, agoB, name) {
+    return `'${name}', cast(sum(iff(${agoCol} between ${agoA} and ${agoB}, ${valueCol}, 0))/${agoB-agoA+1} as int)`
+  }
+
+  var cols = [
+    c(0, 1, 'd2'),
+    c(2, 3, 'd2p'),
+    c(0, 6, 'd7'),
+    c(7, 13, 'd7p'),
+    c(0, 29, 'd30'),
+    c(30, 59, 'd30p'),
+    c(0, 364, 'd365'),
+    c(365, 729, 'd365p')
+  ]
+
+  return `object_construct(${cols.join('\n,')}) as ${alias}`
+}
+
+module.exports = { categories, recCatColumns, periodStatsObject };
