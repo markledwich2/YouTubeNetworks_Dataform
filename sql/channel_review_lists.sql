451f0195702f1f14ae0276ce92238f5e9db94a71
Mark Ledwich <google-oauth2|112278319354366421989>
2021-02-09 10:02:35 +0000

    new alt platform lists

--- sql/channel_review_lists.sql
+++ sql/channel_review_lists.sql
@@ -1,73 +1,59 @@
 with file_lists as (
-  select $1::string as channel_id
-  , regexp_substr(metadata$filename, '([\\w.]+)\.txt', 1, 1, 'e') as list
+  select $1::string channel_id
+       , regexp_substr(metadata$filename,'([\\w.]+)\.txt',1,1,'e') list
   from @public.yt_data/import/review_lists (file_format => tsv)
 )
-   , random_under_reviewed as (
-  select *
-  from (
-         select channel_id, 'Random under-human-reviewed' as list
-         from channel_latest
-         where reviews_human<2
-           and meets_sub_criteria
-       )
-         sample (:limit rows)
-)
-   , popular_under_reviewed as (
-  select channel_id, 'Top viewed under-human-reviewed' as list
+  , popular_under_reviewed as (
+  select channel_id, 'Top viewed under-human-reviewed' list
   from channel_latest
   where reviews_human<1
     and meets_sub_criteria
   order by channel_views desc
   limit :limit
 )
-   , small_tags as (
-  select array_agg(tag) top_tags
-  from (
-         select f.value::string as tag, count(*) count
-         from channel_latest
-            , table ( flatten(tags) ) f
-         where reviews_human>0
-         group by tag
-         order by count
-        limit 8
-       )
-)
-   , popular_non_reviewed_tag as (
-  select channel_id, 'Top viewed algo identified under-human-reviewed tags (bottom 8 tags)' as list
-  from channel_latest c
-         join small_tags t on arrays_overlap(c.tags, t.top_tags)
-  where reviews_algo>0
-    and reviews_human=0
-    and meets_sub_criteria
-  order by channel_views desc
-  limit :limit
-)
   , whide_id_under_reviewed as (
-     select channel_id
-            , concat(array_to_string(
-                array_intersection(array_construct('WhiteIdentitarian', 'QAnon'), c.tags), '|')
-                , ' not-reviewed - '
-                , mod(abs(hash(c.channel_id)), 4)) as list
-     from channel_accepted c
-     where arrays_overlap(array_construct('WhiteIdentitarian', 'QAnon'), c.tags) and reviews_human < 1
+  select channel_id
+       , concat(array_to_string(
+                    array_intersection(array_construct('WhiteIdentitarian','QAnon'),c.tags),'|')
+    ,' not-reviewed - '
+    ,mod(abs(hash(c.channel_id)),4)) list
+  from channel_accepted c
+  where arrays_overlap(array_construct('WhiteIdentitarian','QAnon'),c.tags)
+    and reviews_human<1
 )
-   , u as (
-  select * from file_lists
-  union all select *  from random_under_reviewed
-  union all select * from popular_under_reviewed
-  union all select * from popular_non_reviewed_tag
-  union all select * from whide_id_under_reviewed
+  , top_alts as (
+  select channel_id
+       , concat('Most popular on ',platform,' sans-review - ',mod(abs(hash(c.channel_id)),2)) list
+  from channel_latest c
+  where reviews_human<1
+    and platform<>'YouTube'
+  order by channel_video_views desc
 )
-, s as (
-  select u.channel_id, u.list
+  , u as (
+  select *
+  from file_lists
+  union all
+  select *
+  from popular_under_reviewed
+  union all
+  select *
+  from whide_id_under_reviewed
+  union all
+  select *
+  from top_alts
+)
+  , s as (
+  select u.channel_id
+       , u.list
        , channel_title
        , description
        , logo_url
        , reviews_algo
        , reviews_all
        , channel_views
+       , platform
   from u
          left join channel_latest c on c.channel_id=u.channel_id
 )
-select * from s
+select *
+from s
