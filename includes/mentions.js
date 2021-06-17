6b0e93eba478ed06e06152d5337b74de0fe05831
Mark Ledwich <google-oauth2|112278319354366421989>
2021-06-17 11:42:44 +0000

    narrative telco minor changes

--- includes/mentions.js
+++ includes/mentions.js
@@ -28,8 +28,25 @@ const arraySql = (arr) => `array_construct(${arr.map(a => `'${a.replace(/'/g, "\
  Terms param: array of string terms. Or a tuple that returns a tuple in the form: [<name to use as the match>, a function returning a boolean extpression given the name of a column]
  returns a select query that returns matches at the video granularity.
  columns: video_id, matches, context, part, offset_secconds */
-function mentionsSelect(terms, video_table) {
+function mentionsSelect(terms, video_table, parts) {
+  parts = parts || ['caption', 'description', 'title', 'keyword']
   video_table = video_table || 'video_latest'
+
+  const partToSelect = {
+    caption: select_context(terms, 'mentions_select_cap', 'caption', null, ['offset_seconds::int offset_seconds']),
+    title: select_context(terms, 'mentions_select_vid', 'video_title', 'title', ['null offset_seconds']),
+    description: select_context(terms, 'mentions_select_vid', 'description', null, ['null offset_seconds']),
+    keyword: `  (
+    with mentions_select_kw as (
+      select video_id, video_title, k.value::string keyword
+      from mentions_select_vid, lateral flatten(input => keywords) k
+    )
+    ${select_context(terms, 'mentions_select_kw', 'keyword', null, ['null offset_seconds'])}
+  )`
+  }
+
+
+
   return `
 (
   with mentions_select_vid as (
@@ -41,19 +58,7 @@ function mentionsSelect(terms, video_table) {
       select s.video_id, s.caption, offset_seconds from caption s
       join mentions_select_vid v on v.video_id = s.video_id -- filter via vid table
   )
-  ${select_context(terms, 'mentions_select_cap', 'caption', null, ['offset_seconds::int offset_seconds'])}
-  union all
-  ${select_context(terms, 'mentions_select_vid', 'video_title', 'title', ['null offset_seconds'])}
-  union all
-  ${select_context(terms, 'mentions_select_vid', 'description', null, ['null offset_seconds'])}
-  union all
-  (
-    with mentions_select_kw as (
-      select video_id, video_title, k.value::string keyword
-      from mentions_select_vid, lateral flatten(input => keywords) k
-    )
-    ${select_context(terms, 'mentions_select_kw', 'keyword', null, ['null offset_seconds'])}
-  )
+  ${parts.map(p => partToSelect[p]).join('\n union all \n')}
 )
 `
 }
